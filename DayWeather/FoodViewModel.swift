d0a3d2966d01b147cad4059b2f92c0d89eac8b4f
Jack Lee <dongjunlee222@gmail.com>
2023-10-04 07:15:48 +0900

    [Feat]: 네이버 지도 지역 검색 모델 구축

--- DayWeather/FoodViewModel.swift
+++ DayWeather/FoodViewModel.swift
@@ -9,6 +9,8 @@ import Foundation
 
 class FoodViewModel {
     var temperature: String?
+    private var dataModel: FoodModel?
+    private var term = ""
     
     private var weatherDataManager = WeatherDataManager.shared // 싱글턴 인스턴스 사용
     
@@ -22,5 +24,42 @@ class FoodViewModel {
             self?.temperature = temp
             completion()
         }
+    }
+    
+    func requestAPI() {
+        let sessionConfig = URLSessionConfiguration.default
+        let session = URLSession(configuration: sessionConfig)
+        
+        var baseURL = URLComponents(string: "https://openapi.naver.com/v1/search/local")
+        let param = URLQueryItem(name: "query", value: term)
+        let display = URLQueryItem(name: "display", value: "100")
+        
+        baseURL?.queryItems = [param, display]
+        
+        guard let url = baseURL?.url else { return }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "Get"
+        request.setValue("5jZ3ukktUdvCAob0qT3g", forHTTPHeaderField: "X-Naver-Client-Id")
+        request.setValue("7GcxPHxBai", forHTTPHeaderField: "X-Naver-Client-Secret")
+        
+        let task = session.dataTask(with: request) { data, response, error in
+            print((response as! HTTPURLResponse).statusCode)
+            
+            if let hasData = data {
+                do {
+                    self.dataModel = try JSONDecoder().decode(FoodModel.self, from: hasData)
+                    DispatchQueue.main.async {
+                        // 가져온 데이터 어떻게 처리를 할까
+                        print("마! 이게 데이터다! \(hasData)")
+//                        self.tableView.reloadData()
+                    }
+                } catch {
+                    print("오류가 발생했습니다. \(error.localizedDescription)")
+                }
+            }
+        }
+        task.resume()
+        session.finishTasksAndInvalidate()
     }
 }
