dd48229bec8d62a0df9b9b3890a07f00c957b09a
Jack Lee <dongjunlee222@gmail.com>
2023-10-05 16:25:09 +0900

    [Feat]: 네이버 검색 모델 추가, 기존 GeoCode 모델 일부 수정

--- DayWeather/Model/QueryModel.swift
+++ DayWeather/Model/QueryModel.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+struct QueryModel: Codable {
+    let lastBuildDate: String
+    var total:Int
+    var start:Int
+    var display:Int
+    let items: [Item]
+}
+
+// MARK: - Item
+struct Item: Codable {
+    let title: String
+    let address: String
+}
